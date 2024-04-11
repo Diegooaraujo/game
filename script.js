ada0a288c1236dc28fd2b85540ba2b9d3edad6d4
Diegooaraujo <149812280+Diegooaraujo@users.noreply.github.com>
2024-04-11 15:52:01 -0300

    add events

--- script.js
+++ script.js
@@ -1,32 +1,100 @@
 const escolher_equipe = document.querySelector(".escolher-equipes")
-const equipeUm = document.querySelector("#NomeEquipeUm")
-const equipeDois = document.querySelector("#NomeEquipeDois")
+const equipeUm = document.querySelector("#nomeEquipeUm")
+const equipeDois = document.querySelector("#nomeEquipeDois")
 const btnEnviarNome = document.querySelector("#enviarNome")
+const conteiner = document.querySelector(".conteiner-full")
+const passa_repassa = document.querySelector(".passa-repassa")
+const start = document.querySelector(".start")
+const comecar = document . querySelector("#comecar")
+const respostas = document.querySelector(".bottom")
+const topo = document.querySelector(".top")
+
+
 let mudarTela = false
 
-btnEnviarNome.addEventListener("Click",(e)=>{
-    
-    if(equipeUm.value!=equipeUm && equipeDois.value!=equipeDois){
-        let nomeEquipeUm = equipeUm.value
-        let nomeEquipeDois = equipeDois.value
-        mudarTela = true
+conteiner.style.display ="none"
+passa_repassa.style.display ="none"
+respostas.style.display="none"
 
-        
+
+
+btnEnviarNome.addEventListener("click",(e)=>{
+    e.preventDefault()
+    if(equipeUm.value==""&& equipeDois.value==""){
+        alert("digite os nomes dos times")
+    }else{
+        pegar_nome()
     }
     
+
+    
 })
-if(mudarTela){
-    escolher_equipe.style.display="none"
-    conteiner.style.display ="flex"
+
+
+/*colocar os nomes dos times no header*/
+const timeUm = document.querySelector("#timeUm")
+const timeDois = document.querySelector("#timeDois")
+const equipeStart = document.querySelector("#equipeResponde")
+const mudar_nome_header = function(nomeUm,nomeDois){
+    timeUm.innerText =nomeUm
+    timeDois.innerText = nomeDois
+    equipeStart.innerText = ` ${nomeUm} responde`
+}
+
+/*pegar os nomes e mudar tela*/
+const pegar_nome = function(){
+    if(equipeUm!=null && equipeDois!=null){
+        const nomeEquipeUm = equipeUm.value
+        const nomeEquipeDois = equipeDois.value
+        mudar_nome_header(nomeEquipeUm,nomeEquipeDois)
+    }
+    mudarTela = true
+    if(mudarTela){
+        escolher_equipe.style.display="none"
+        start.style.display = "flex"
+        /*conteiner.style.display ="flex*/
+        /*passa_repassa.style.display = "flex"*/
+    }
+}
+//começar 
+const mudar_tela_perguntas = function(){
+    if(mudarTela){
+        conteiner.style.display ="flex"
+        start.style.display= "none"
+        topo.style.height ="100%"
+
+    }
+}
+comecar.addEventListener("click",()=>{
+    mudarTela = true
+    mudar_tela_perguntas()
+    analise()
+    
+    
+})
+
+
+//ver quem clicou primeiro
+
+function analise (){
+        document.body.addEventListener("keyup",(e)=>{
+        console.log(e.code)
+            
+    })
 }
 
 
 
-const conteiner = document.querySelector(".conteiner")
-const passa_repassa = document.querySelector(".passa-repassa")
 
 
 
-conteiner.style.display ="none"
-passa_repassa.style.display ="none"
+
+
+
+
+
+
+
+
+
 
